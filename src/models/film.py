ba1a4854c1d594be19c626b08a1293ab5535e6fe
Pavel Petrikovskiy <petrikovskiy@lanit.ru>
2021-07-18 13:56:10 +0300

    Adding models for task #3

--- src/models/film.py
+++ src/models/film.py
@@ -1,18 +1,53 @@
 import orjson
 
-# Используем pydantic для упрощения работы при перегонке данных из json в объекты
+from typing import Dict, List
+
 from pydantic import BaseModel
 
+
 def orjson_dumps(v, *, default):
-    # orjson.dumps возвращает bytes, а pydantic требует unicode, поэтому декодируем
     return orjson.dumps(v, default=default).decode()
 
-class Film(BaseModel):
-    id: str
-    title: str
-    description: str
+
+class LocalBaseModel(BaseModel):
 
     class Config:
-        # Заменяем стандартную работу с json на более быструю
         json_loads = orjson.loads
         json_dumps = orjson_dumps
+
+
+class Film(LocalBaseModel):
+    id: str
+    title: str
+    work_type: str
+    description: str
+    imdb_rating: float
+    creation_date: str
+    actors: Dict[str, str]
+    directors: Dict[str, str]
+    writers: Dict[str, str]
+    actors_names: List[str]
+    directors_names: List[str]
+    writers_names: List[str]
+    genres: List[str]
+
+
+class Genre(LocalBaseModel):
+    name: str
+
+
+class Person(LocalBaseModel):
+    id: str
+    name: str
+
+
+class Actor(Person):
+    profession: str = 'Actor'
+
+
+class Writer(Person):
+    profession: str = 'Writer'
+
+
+class Director(Person):
+    profession: str = 'Director'
